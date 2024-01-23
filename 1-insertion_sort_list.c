90e9714eea01a0272dc5489c8f9e2a1acf73798f
obieomens <obieomens@gmail.com>
2024-01-23 01:14:16 -0800

    updated task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	(*n1)->next = n2->next;
+(*n1)->next = n2->next;
 	if (n2->next != NULL)
 	{
 		n2->next->prev = *n1;
@@ -35,8 +35,8 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+listint_t *iter, *insert, *tmp;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
